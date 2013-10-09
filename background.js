20b3a7d1dcd1e9797a4691f1d2880d7f11172645
Yuval Gnessin <ygnessin@gmail.com>
2013-10-08 22:40:17 -0700

    Changed to page_action from browser_action

---
+++ background.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+	// If the tabs url starts with "http://specificsite.com"...
+	if (tab.url.indexOf("http://gatherer.wizards.com/Pages/Card") == 0) {
+		// ... show the page action.
+		chrome.pageAction.show(tabId);
+	}
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
