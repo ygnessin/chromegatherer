73dbf2e10649149400776af59de32dad7baa65be
Yuval Gnessin <ygnessin@gmail.com>
2013-10-16 12:58:29 -0700

    started working on API access

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
+  // get card name from page text
   var name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow > .value").text();
+  // get card set from page text
   var set  = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_setRow > .value").text();
   
   // get multiverse ID from HTTP params
@@ -8,6 +10,14 @@ $(document).ready(function() {
 
   // get card name from HTTP params
   // var name = getURLParameter("name");
+
+  // Query TCGplayer's API
+  var pk = "GATHERERPRICES"
+  var url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=" + set + "&p=" + name
+  $.get( url, function( xml ) {
+    alert(xml)
+    $(".leftCol").append("<div>" + xml + "</div>");   
+  });
 
   $(".leftCol").append("<div>" + name + "</div>");
   $(".leftCol").append("<div>" + set + "</div>");
