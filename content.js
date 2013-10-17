a4a35a02abefe22d562f890637007c9dfee1bedc
Yuval Gnessin <ygnessin@gmail.com>
2013-10-16 21:59:11 -0700

    Added full API access. Styling still required, as well as pop-up implementation

--- content.js
+++ content.js
@@ -2,33 +2,42 @@ $(document).ready(function() {
 
   // get card name from page text
   var name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow > .value").text();
+  name = $.trim(name)
   // get card set from page text
   var set  = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_setRow > .value").text();
-  
-  // get multiverse ID from HTTP params
-  // var id = getURLParameter("multiverseid");
+  set = $.trim(set)
 
-  // get card name from HTTP params
-  // var name = getURLParameter("name");
+  var hiprice = null
+  var lowprice = null
+  var avgprice = null
+  var foilavgprice = null
+  var storelink = null
+
+  $(".leftCol").append('<div class="price" id="hiprice">hiprice</div>');
+  $(".leftCol").append('<div class="price" id="avgprice">avgprice</div>');
+  $(".leftCol").append('<div class="price" id="lowprice">lowprice</div>');
+  $(".leftCol").append('<div class="price" id="foilavgprice">foilavgprice</div>');
 
   // Query TCGplayer's API
-  var pk = "GATHERERPRICES"
+  var pk = "GATHERPRICES"
   var url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=" + set + "&p=" + name
   $.get( url, function( xml ) {
-    alert(xml)
-    $(".leftCol").append("<div>" + xml + "</div>");   
-  });
+    hiprice = $(xml).find("hiprice").text();
+    lowprice = $(xml).find("lowprice").text();
+    avgprice = $(xml).find("avgprice").text();
+    foilavgprice = $(xml).find("foilavgprice").text();
+    storelink = $(xml).find("link").text();
 
-  $(".leftCol").append("<div>" + name + "</div>");
-  $(".leftCol").append("<div>" + set + "</div>");
+    $(".leftCol").append("<div>" + avgprice + "</div>");
+  });
 
   // send message to the extension containing the card ID and Name
   chrome.runtime.sendMessage(
   {
-    cardinfo: 
+    cardinfo:
     {
-      cardname: name, 
-      cardset:  set
+      cardname: name,
+      cardset:  set,
     }
   }, function(response) {
     // do stuff?
