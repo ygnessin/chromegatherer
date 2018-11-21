eaa454759f6311bb0ed7a51f0aa006ba335a467b
Yuval Gnessin <yuval@autolist.com>
2018-11-21 03:24:13 -0800

    add more mappings for weird set names

--- set_map.js
+++ set_map.js
@@ -8,5 +8,11 @@ const SET_MAP = {
   'Planechase 2012 Edition': 'Planechase 2012',
   'Commander 2013 Edition': 'Commander 2013',
   'Time Spiral "Timeshifted"': 'Timeshifted',
-  'Promo set for Gatherer': 'Media Promos'
+  'Promo set for Gatherer': 'Media Promos',
+  'Guilds of Ravnica Mythic Edition': 'Guilds of Ravnica: Mythic Edition',
+  'Commander Anthology 2018': 'Commander Anthology Volume II',
+  'Modern Masters 2017 Edition': 'Modern Masters 2017',
+  'Modern Masters 2015 Edition': 'Modern Masters 2015',
+  'Game Night': 'Magic Game Night',
+  'Global Series: Jiang Yanggu and Mu Yanling': 'Global Series Jiang Yanggu %26 Mu Yanling'
 };
