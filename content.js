3385c5548e1e6f655997d0ee9e0c95cb22f44b75
Yuval Gnessin <yuval@autolist.com>
2018-11-21 01:32:46 -0800

    Some refactoring

--- content.js
+++ content.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
 
   // Handle double-sided cards and split cards
   var second_name = '';
-  if ( name.text().length == 0 ) {
+  if (name.text().length == 0) {
     name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl07_nameRow > .value");
     set = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl07_setRow > .value");
 
     // Set up Split card names in case this is a Split card
     second_name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl08_nameRow > .value");
     // Sometimes split cards use ct109 and ct110
-    if ( name.text().length == 0 ) {
+    if (name.text().length == 0) {
       name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl09_nameRow > .value");
       set = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl09_setRow > .value");
       second_name = $("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ctl10_nameRow > .value");
@@ -25,7 +25,7 @@ $(document).ready(function() {
   // trim spaces and handle AE characters
   name = escape($.trim(name.text())).replace('%C6', 'AE');
   set = $.trim(set.text());
-  
+
   var split_name_1 = name + '+%2f%2f+' + second_name
   var split_name_2 = second_name + '+%2f%2f+' + name
 
@@ -51,12 +51,12 @@ $(document).ready(function() {
   set_map['Promo set for Gatherer'] = 'Media Promos';
 
   if (set_map[set] != undefined) { set = set_map[set] };
-  
+
   set = set.replace(/Magic: The Gathering\b\S\b/, '');
   set = set.replace(' vs. ', ' vs ');
 
   m = set.match(/Magic 20(\d\d)/);
-  if ( m != null ) {
+  if (m != null) {
     year = m[1];
     if (typeof(year) == "string") { set = "Magic 20" + year + " (M" + year + ")";};
   };
@@ -66,20 +66,19 @@ $(document).ready(function() {
   $(".leftCol").append(price_div);
 
   // Prepare variables needed for API query
-  var pk = "GATHPRICE";
-  var url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=" + set + "&p=" + name;
+  var url = generateTCGPlayerUrl(set, name);
 
   // Query TCGplayer's Hi-Mid-Low API
-  $.get( url, handleAPIResult ).fail( function() {
+  $.get(url, handleAPIResult).fail( function() {
     // If failed, try again, but without Set name (gives average for all sets)
-    url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=&p=" + name;
-    $.get( url, reportAllSets ).fail( function() {
+    url = generateTCGPlayerUrl("", name);
+    $.get(url, reportAllSets).fail( function() {
       // If failed, maybe it's a Split card
-      url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=" + set + "&p=" + split_name_1;
-      $.get( url, handleAPIResult ).fail( function() {
+      url = generateTCGPlayerUrl(set, split_name_1);
+      $.get(url, handleAPIResult).fail( function() {
         // If failed, maybe the split card names are backwards
-        url = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=" + pk + "&s=" + set + "&p=" + split_name_2;
-        $.get( url, handleAPIResult ).fail( function() {
+        url = generateTCGPlayerUrl(set, split_name_2);
+        $.get(url, handleAPIResult).fail( function() {
           // If still failed, give up and print an error
           $(".leftCol").append('<div id="error">Sorry, an error occured fetching price data</div>');
         });
@@ -101,7 +100,11 @@ $(document).ready(function() {
 
 })
 
-function handleAPIResult( xml ) {
+function generateTCGPlayerUrl(set, cardName) {
+  return "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=GATHPRICE&s=" + set + "&p=" + cardName;
+}
+
+function handleAPIResult(xml) {
   hiprice = "$" + $(xml).find("hiprice").text();
   lowprice = "$" + $(xml).find("lowprice").text();
   avgprice = "$" + $(xml).find("avgprice").text();
@@ -125,14 +128,9 @@ function handleAPIResult( xml ) {
 
   // Set the widths for the price elements based on the price div
   stylePrices();
-
-  // Add store link
-  // $('.leftCol').append(
-  //   '<div id="storelink"><a href="' + storelink +'">Buy ' + name + '</div></a>'
-  // );
 }
 
-function reportAllSets( xml ) {
+function reportAllSets(xml) {
   handleAPIResult(xml);
   $('.price').css('font-style', 'italic')
   var footnote_msg = 'These prices are aggregated over all available printings of this card, because the printing you selected does not have any available pricing data.';
@@ -158,7 +156,7 @@ function stylePrices() {
 
   if (rnd == cl) { $('.avgprice').css('width', fl) };
   if (rnd == fl) { $('.avgprice').css('width', cl) };
-  
+
   // The new Gatherer layout has weird font color handling, so we need to do this here
   $('.price a').css('color', 'black');
 }
